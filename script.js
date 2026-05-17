ced59113c4d229509cccb27d4218717765dc10cc
meatsoda <meatsoda@meatsoda.is>
2026-05-17 15:37:17 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,101 @@
+const header = document.getElementById("header");
+const topMarker = document.getElementById("topMarker");
+const banner = document.getElementById("titleBanner");
+
+const graphicsMarker = document.getElementById("graphicsMarker");
+const logosMarker = document.getElementById("logosMarker");
+const motionMarker = document.getElementById("motionMarker");
+
+const graphNav = document.getElementById("graphicsNav");
+const logoNav = document.getElementById("logosNav");
+const motionNav = document.getElementById("motionNav");
+
+const observer = new IntersectionObserver(([entry]) => {
+    if (entry.isIntersecting) {
+        header.classList.remove("reveal");
+        banner.classList.remove("hide");
+        
+        if (contactState == true) {
+            contact.classList.remove("reveal");
+            contactState = false;
+        }
+    } else {
+        header.classList.add("reveal");
+        banner.classList.add("hide");
+        
+        if (landingContactState == true) {
+            landingContact.classList.remove("reveal");
+            landingContactButton.classList.remove("highlight");
+            landingContactState = false;
+        }
+        
+    }
+});
+
+const graphObserver = new IntersectionObserver(([entry]) => {
+    
+    if (entry.isIntersecting) {
+        graphNav.classList.add("highlight");
+    } else {
+        graphNav.classList.remove("highlight");
+    }
+});
+
+const logoObserver = new IntersectionObserver(([entry]) => {
+    
+    if (entry.isIntersecting) {
+        logoNav.classList.add("highlight");
+    } else {
+        logoNav.classList.remove("highlight");
+    }
+});
+
+const motionObserver = new IntersectionObserver(([entry]) => {
+    
+    if (entry.isIntersecting) {
+        motionNav.classList.add("highlight");
+    } else {
+        motionNav.classList.remove("highlight");
+    }
+});
+
+observer.observe(topMarker);
+
+graphObserver.observe(graphicsMarker);
+logoObserver.observe(logosMarker);
+motionObserver.observe(motionMarker);
+
+let contactState = false;
+
+const contact = document.getElementById("contactSheet");
+
+function contactSheet() {
+    
+    if (contactState == false) {
+        contact.classList.add("reveal");
+        contactState = true;
+    } else if (contactState == true) {
+        contact.classList.remove("reveal");
+        contactState = false;
+    }
+}
+
+let landingContactState = false;
+
+const landingContact = document.getElementById("landingContactSheet");
+const landingContactButton = document.getElementById("landingContact");
+
+function contactLandingSheet() {
+    
+    if (landingContactState == false) {
+        landingContact.classList.add("reveal");
+        landingContactButton.classList.add("highlight");
+        landingContactState = true;
+    } else if (landingContactState == true) {
+        landingContact.classList.remove("reveal");
+        landingContactButton.classList.remove("highlight");
+        landingContactState = false;
+    }
+}
+document.getElementById("emailText").innerHTML = "meatsoda@meatsoda.is";
+document.getElementById("emailLandingText").innerHTML = "meatsoda@meatsoda.is";
